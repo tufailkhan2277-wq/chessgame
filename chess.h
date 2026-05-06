979c3c699ac27c61efad77681d2e833568fe0a79
tufailkhan2277-wq <tufailkhan2277@gmail.com>
2026-05-06 18:30:20 +0500

    Added pawn class
    in this commit i just declared the pawn class

--- chess.h
+++ chess.h
@@ -1,15 +1,50 @@
-#pragma once
-#ifndef chess_H
-#define chess_H
+#ifndef CHESS_H
+#define CHESS_H
 
-class chess
-{
-private:
-	char chessBoard[8][8];
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum Color { WHITE, BLACK };
+
+struct Position {
+	int r, c;
+	Position(int r = 0, int c = 0);
+};
+
+class Board;
+
+class Piece {
+protected:
+	Color color;
 
 public:
-	void initialize();
-	void display();
+	Piece(Color c);
+	virtual ~Piece();
+
+	Color getColor();
+
+	virtual char symbol() = 0;
+	virtual bool canMove(Position, Position, Board &) = 0;
 };
+
+class Board {
+public:
+	Piece *grid[8][8];
+
+	Board();
+	~Board();
+
+	Piece *get(Position p);
+	void set(Position p, Piece *pc);
+	void move(Position f, Position t);
+	void draw();
+};
+
+int absVal(int x);
+
+Position parse(string s);
+string toNotation(Position p);
+void setupBoard(Board &board);
 
 #endif
