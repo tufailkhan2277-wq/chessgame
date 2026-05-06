560d5a688492f8bbe8d836c4a89b8829a338abe5
tufailkhan2277-wq <tufailkhan2277@gmail.com>
2026-05-06 18:56:56 +0500

    Added move input system and turn handling

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #include "chess.h"
 
 int main() {
+
 	Board board;
 
 	setupBoard(board);
-	board.draw();
+
+	Color turn = WHITE;
+
+	string a, b;
+
+	while (true) {
+
+		board.draw();
+
+		if (turn == WHITE) {
+			cout << "\nWhite move: ";
+		}
+		else {
+			cout << "\nBlack move: ";
+		}
+
+		cin >> a;
+
+		if (a == "quit") {
+			break;
+		}
+
+		cin >> b;
+
+		Position f = parse(a);
+		Position t = parse(b);
+
+		Piece *p = board.get(f);
+
+		if (p == nullptr) {
+			cout << "Invalid move" << endl;
+			continue;
+		}
+
+		if (p->getColor() != turn) {
+			cout << "Wrong turn" << endl;
+			continue;
+		}
+
+		if (!p->canMove(f, t, board)) {
+			cout << "Invalid move" << endl;
+			continue;
+		}
+
+		board.move(f, t);
+
+		if (turn == WHITE) {
+			turn = BLACK;
+		}
+		else {
+			turn = WHITE;
+		}
+	}
 
 	return 0;
 }
