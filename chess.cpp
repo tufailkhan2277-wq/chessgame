3fc5f0a4cf1a0471b90e6f9524942988b37d43e8
tufailkhan2277-wq <tufailkhan2277@gmail.com>
2026-05-06 18:29:13 +0500

    added pawn class definitions
    in this commit i just added the definitions of the pawn class

--- chess.cpp
+++ chess.cpp
@@ -1,61 +1,102 @@
-#include <iostream>
 #include "chess.h"
 
-using namespace std;
+Position::Position(int r, int c) {
+	this->r = r;
+	this->c = c;
+}
 
-void chess::initialize()
-{
-	// this is for empty spaces
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			chessBoard[i][j] = '.';
+Piece::Piece(Color c) {
+	color = c;
+}
+
+Piece::~Piece() {}
+
+Color Piece::getColor() {
+	return color;
+}
+
+Board::Board() {
+	for (int r = 0; r < 8; r++) {
+		for (int c = 0; c < 8; c++) {
+			grid[r][c] = nullptr;
 		}
-	}
-
-	// this is for black pieces
-	chessBoard[0][0] = 'R';
-	chessBoard[0][1] = 'N';
-	chessBoard[0][2] = 'B';
-	chessBoard[0][3] = 'Q';
-	chessBoard[0][4] = 'K';
-	chessBoard[0][5] = 'B';
-	chessBoard[0][6] = 'N';
-	chessBoard[0][7] = 'R';
-
-	for (int i = 0; i < 8; i++)
-	{
-		chessBoard[1][i] = 'P';
-	}
-
-	// this is for white pieces
-	chessBoard[7][0] = 'r';
-	chessBoard[7][1] = 'n';
-	chessBoard[7][2] = 'b';
-	chessBoard[7][3] = 'q';
-	chessBoard[7][4] = 'k';
-	chessBoard[7][5] = 'b';
-	chessBoard[7][6] = 'n';
-	chessBoard[7][7] = 'r';
-
-	for (int i = 0; i < 8; i++)
-	{
-		chessBoard[6][i] = 'p';
 	}
 }
 
-void chess::display()
-{
-	cout << "\nChess Board:\n" << endl;
+Board::~Board() {
+	for (int r = 0; r < 8; r++) {
+		for (int c = 0; c < 8; c++) {
+			delete grid[r][c];
+		}
+	}
+}
 
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			cout << chessBoard[i][j] << " ";
+Piece *Board::get(Position p) {
+	if (p.r < 0 || p.r > 7 || p.c < 0 || p.c > 7) {
+		return nullptr;
+	}
+
+	return grid[p.r][p.c];
+}
+
+void Board::set(Position p, Piece *pc) {
+	grid[p.r][p.c] = pc;
+}
+
+void Board::move(Position f, Position t) {
+	if (grid[t.r][t.c] != nullptr) {
+		delete grid[t.r][t.c];
+	}
+
+	grid[t.r][t.c] = grid[f.r][f.c];
+	grid[f.r][f.c] = nullptr;
+}
+
+void Board::draw() {
+	cout << endl << "  a b c d e f g h" << endl;
+
+	for (int r = 0; r < 8; r++) {
+		cout << 8 - r << " ";
+
+		for (int c = 0; c < 8; c++) {
+			if (grid[r][c] != nullptr) {
+				cout << grid[r][c]->symbol() << " ";
+			}
+			else {
+				cout << ". ";
+			}
 		}
 
-		cout << endl;
+		cout << 8 - r << endl;
 	}
+
+	cout << "  a b c d e f g h" << endl;
+}
+
+int absVal(int x) {
+	if (x < 0) {
+		return -x;
+	}
+
+	return x;
+}
+
+Position parse(string s) {
+	int r = 8 - (s[1] - '0');
+	int c = s[0] - 'a';
+
+	return Position(r, c);
+}
+
+string toNotation(Position p) {
+	string s = "";
+
+	s += char('a' + p.c);
+	s += char('0' + (8 - p.r));
+
+	return s;
+}
+
+void setupBoard(Board &board) {
+	// pieces will be added in next commits
 }
