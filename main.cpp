0b79d8e081cecddf2431fae6f493fb31abbd3b07
Abdul-Muqeet602 <p240716@cfd.nu.edu.pk>
2026-05-10 18:06:12 +0500

    Helper function for current turn

--- main.cpp
+++ main.cpp
@@ -10,26 +10,18 @@ void printCommands() {
 
 void printLegalMoves(Board &board, Position f) {
 	Piece *p = board.get(f);
-
 	cout << "Legal moves for " << p->symbol() << " at " << toNotation(f) << ": ";
-
 	bool found = false;
-
 	for (int r = 0; r < 8; r++) {
 		for (int c = 0; c < 8; c++) {
 			Position t(r, c);
-
 			if (p->canMove(f, t, board)) {
 				cout << toNotation(t) << " ";
 				found = true;
 			}
 		}
 	}
-
-	if (!found) {
-		cout << "(none)";
-	}
-
+	if (!found) cout << "(none)";
 	cout << endl;
 }
 
@@ -43,76 +35,50 @@ void printTurn(Color turn) {
 }
 
 int main() {
-
 	Board board;
-
 	setupBoard(board);
-
 	Color turn = WHITE;
-
 	string a, b;
 
 	while (true) {
-
 		board.draw();
-
 		printCommands();
-
 		printTurn(turn);
 		
 		cin >> a;
-
-		if (a == "quit") {
-			break;
-		}
-
+		if (a == "quit") break;
 		if (a == "help") {
 			printCommands();
 			continue;
 		}
-
 		if (a == "moves") {
 			cin >> b;
 			Position f = parse(b);
 			Piece *p = board.get(f);
-
-			if (p != nullptr) {
-				printLegalMoves(board, f);
-			}
+			if (p != nullptr) printLegalMoves(board, f);
 			continue;
 		}
 
 		cin >> b;
-
 		Position f = parse(a);
 		Position t = parse(b);
-
 		Piece *p = board.get(f);
 
 		if (p == nullptr) {
 			cout << "Invalid move" << endl;
 			continue;
 		}
-
 		if (p->getColor() != turn) {
 			cout << "Wrong turn" << endl;
 			continue;
 		}
-
 		if (!p->canMove(f, t, board)) {
 			cout << "Invalid move" << endl;
 			continue;
 		}
 
 		board.move(f, t);
-
-		if (turn == WHITE) {
-			turn = BLACK;
-		}
-		else {
-			turn = WHITE;
-		}
+		turn = (turn == WHITE) ? BLACK : WHITE;
 	}
-
 	return 0;
 }
