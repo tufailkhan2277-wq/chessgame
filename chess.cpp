b73ad0328ff34982f9f6d6a88acc07e82aefad78
Abdul-Muqeet602 <p240716@cfd.nu.edu.pk>
2026-05-10 17:42:56 +0500

    Add board boundary helper function

--- chess.cpp
+++ chess.cpp
@@ -32,10 +32,9 @@ Board::~Board() {
 }
 
 Piece *Board::get(Position p) {
-	if (p.r < 0 || p.r > 7 || p.c < 0 || p.c > 7) {
+	if (!isInside(p)) {
 		return nullptr;
 	}
-
 	return grid[p.r][p.c];
 }
 
@@ -47,17 +46,14 @@ void Board::move(Position f, Position t) {
 	if (grid[t.r][t.c] != nullptr) {
 		delete grid[t.r][t.c];
 	}
-
 	grid[t.r][t.c] = grid[f.r][f.c];
 	grid[f.r][f.c] = nullptr;
 }
 
 void Board::draw() {
 	cout << endl << "  a b c d e f g h" << endl;
-
 	for (int r = 0; r < 8; r++) {
 		cout << 8 - r << " ";
-
 		for (int c = 0; c < 8; c++) {
 			if (grid[r][c] != nullptr) {
 				cout << grid[r][c]->symbol() << " ";
@@ -66,10 +62,8 @@ void Board::draw() {
 				cout << ". ";
 			}
 		}
-
 		cout << 8 - r << endl;
 	}
-
 	cout << "  a b c d e f g h" << endl;
 }
 
@@ -77,263 +71,142 @@ int absVal(int x) {
 	if (x < 0) {
 		return -x;
 	}
-
 	return x;
 }
-Pawn::Pawn(Color c) : Piece(c) {}
 
-char Pawn::symbol() {
-	if (color == WHITE) {
-		return 'P';
+bool isInside(Position p) {
+	if (p.r >= 0 && p.r < 8 && p.c >= 0 && p.c < 8) {
+		return true;
 	}
-
-	return 'p';
+	return false;
 }
 
+Pawn::Pawn(Color c) : Piece(c) {}
+char Pawn::symbol() {
+	return (color == WHITE) ? 'P' : 'p';
+}
 bool Pawn::canMove(Position f, Position t, Board &b) {
-	int d;
-
-	if (color == WHITE) {
-		d = -1;
-	}
-	else {
-		d = 1;
-	}
-
+	int d = (color == WHITE) ? -1 : 1;
 	if (f.c == t.c && b.get(t) == nullptr && t.r == f.r + d) {
 		return true;
 	}
-
 	if (f.c == t.c && b.get(t) == nullptr) {
 		if (color == WHITE) {
-			if (f.r == 6 && t.r == 4 && b.get(Position(5, f.c)) == nullptr) {
-				return true;
-			}
+			if (f.r == 6 && t.r == 4 && b.get(Position(5, f.c)) == nullptr) return true;
 		}
 		else {
-			if (f.r == 1 && t.r == 3 && b.get(Position(2, f.c)) == nullptr) {
-				return true;
-			}
+			if (f.r == 1 && t.r == 3 && b.get(Position(2, f.c)) == nullptr) return true;
 		}
 	}
-
 	if (absVal(f.c - t.c) == 1 && t.r == f.r + d) {
 		Piece *p = b.get(t);
-
-		if (p != nullptr && p->getColor() != color) {
-			return true;
-		}
+		if (p != nullptr && p->getColor() != color) return true;
 	}
-
 	return false;
 }
+
 Rook::Rook(Color c) : Piece(c) {}
-
 char Rook::symbol() {
-	if (color == WHITE) {
-		return 'R';
-	}
-
-	return 'r';
+	return (color == WHITE) ? 'R' : 'r';
 }
-
 bool Rook::canMove(Position f, Position t, Board &b) {
-	if (f.r != t.r && f.c != t.c) {
-		return false;
-	}
-
-	int dr = 0;
-	int dc = 0;
-
-	if (t.r > f.r) dr = 1;
-	else if (t.r < f.r) dr = -1;
-
-	if (t.c > f.c) dc = 1;
-	else if (t.c < f.c) dc = -1;
-
+	if (f.r != t.r && f.c != t.c) return false;
+	int dr = (t.r > f.r) ? 1 : (t.r < f.r ? -1 : 0);
+	int dc = (t.c > f.c) ? 1 : (t.c < f.c ? -1 : 0);
 	Position p(f.r + dr, f.c + dc);
-
 	while (p.r != t.r || p.c != t.c) {
-		if (b.get(p) != nullptr) {
-			return false;
-		}
-
+		if (b.get(p) != nullptr) return false;
 		p.r += dr;
 		p.c += dc;
 	}
-
 	Piece *q = b.get(t);
-
-	if (q == nullptr || q->getColor() != color) {
-		return true;
-	}
-
-	return false;
+	return (q == nullptr || q->getColor() != color);
 }
+
 Knight::Knight(Color c) : Piece(c) {}
-
 char Knight::symbol() {
-	if (color == WHITE) {
-		return 'N';
-	}
-
-	return 'n';
+	return (color == WHITE) ? 'N' : 'n';
 }
-
 bool Knight::canMove(Position f, Position t, Board &b) {
-
 	int dr = absVal(f.r - t.r);
 	int dc = absVal(f.c - t.c);
-
-	if (!((dr == 2 && dc == 1) ||
-		(dr == 1 && dc == 2))) {
-
-		return false;
-	}
-
+	if (!((dr == 2 && dc == 1) || (dr == 1 && dc == 2))) return false;
 	Piece *p = b.get(t);
-
-	if (p == nullptr ||
-		p->getColor() != color) {
-
-		return true;
-	}
-
-	return false;
+	return (p == nullptr || p->getColor() != color);
 }
+
 Bishop::Bishop(Color c) : Piece(c) {}
-
 char Bishop::symbol() {
-	if (color == WHITE) {
-		return 'B';
-	}
-
-	return 'b';
+	return (color == WHITE) ? 'B' : 'b';
 }
-
 bool Bishop::canMove(Position f, Position t, Board &b) {
-	if (absVal(f.r - t.r) != absVal(f.c - t.c)) {
-		return false;
-	}
-
-	int dr;
-	int dc;
-
-	if (t.r > f.r) dr = 1;
-	else dr = -1;
-
-	if (t.c > f.c) dc = 1;
-	else dc = -1;
-
+	if (absVal(f.r - t.r) != absVal(f.c - t.c)) return false;
+	int dr = (t.r > f.r) ? 1 : -1;
+	int dc = (t.c > f.c) ? 1 : -1;
 	Position p(f.r + dr, f.c + dc);
-
 	while (p.r != t.r) {
-		if (b.get(p) != nullptr) {
-			return false;
-		}
-
+		if (b.get(p) != nullptr) return false;
 		p.r += dr;
 		p.c += dc;
 	}
-
 	Piece *q = b.get(t);
-
-	if (q == nullptr || q->getColor() != color) {
-		return true;
-	}
-
-	return false;
+	return (q == nullptr || q->getColor() != color);
 }
+
 Queen::Queen(Color c) : Piece(c) {}
-
 char Queen::symbol() {
-	if (color == WHITE) {
-		return 'Q';
-	}
-
-	return 'q';
+	return (color == WHITE) ? 'Q' : 'q';
 }
-
 bool Queen::canMove(Position f, Position t, Board &b) {
-
 	Rook r(color);
 	Bishop bp(color);
-
-	if (r.canMove(f, t, b)) {
-		return true;
-	}
-
-	if (bp.canMove(f, t, b)) {
-		return true;
-	}
-
-	return false;
+	return (r.canMove(f, t, b) || bp.canMove(f, t, b));
 }
+
 King::King(Color c) : Piece(c) {}
-
 char King::symbol() {
-	if (color == WHITE) {
-		return 'K';
-	}
-
-	return 'k';
+	return (color == WHITE) ? 'K' : 'k';
 }
-
 bool King::canMove(Position f, Position t, Board &b) {
-
-	if (absVal(f.r - t.r) <= 1 &&
-		absVal(f.c - t.c) <= 1) {
-
+	if (absVal(f.r - t.r) <= 1 && absVal(f.c - t.c) <= 1) {
 		Piece *p = b.get(t);
-
-		if (p == nullptr ||
-			p->getColor() != color) {
-
-			return true;
-		}
+		return (p == nullptr || p->getColor() != color);
 	}
-
 	return false;
 }
+
 Position parse(string s) {
 	int r = 8 - (s[1] - '0');
 	int c = s[0] - 'a';
-
 	return Position(r, c);
 }
 
 string toNotation(Position p) {
 	string s = "";
-
 	s += char('a' + p.c);
 	s += char('0' + (8 - p.r));
-
 	return s;
 }
 
-
-	void setupBoard(Board &board) {
-		for (int i = 0; i < 8; i++) {
-			board.set(Position(6, i), new Pawn(WHITE));
-			board.set(Position(1, i), new Pawn(BLACK));
-		}
-
-		board.set(Position(7, 0), new Rook(WHITE));
-		board.set(Position(7, 7), new Rook(WHITE));
-		board.set(Position(0, 0), new Rook(BLACK));
-		board.set(Position(0, 7), new Rook(BLACK));
-		board.set(Position(7, 1), new Knight(WHITE));
-		board.set(Position(7, 6), new Knight(WHITE));
-
-		board.set(Position(0, 1), new Knight(BLACK));
-		board.set(Position(0, 6), new Knight(BLACK));
-		board.set(Position(7, 2), new Bishop(WHITE));
-		board.set(Position(7, 5), new Bishop(WHITE));
-
-		board.set(Position(0, 2), new Bishop(BLACK));
-		board.set(Position(0, 5), new Bishop(BLACK));
-		board.set(Position(7, 3), new Queen(WHITE));
-		board.set(Position(0, 3), new Queen(BLACK));
-		board.set(Position(7, 4), new King(WHITE));
-		board.set(Position(0, 4), new King(BLACK));
+void setupBoard(Board &board) {
+	for (int i = 0; i < 8; i++) {
+		board.set(Position(6, i), new Pawn(WHITE));
+		board.set(Position(1, i), new Pawn(BLACK));
 	}
+	board.set(Position(7, 0), new Rook(WHITE));
+	board.set(Position(7, 7), new Rook(WHITE));
+	board.set(Position(0, 0), new Rook(BLACK));
+	board.set(Position(0, 7), new Rook(BLACK));
+	board.set(Position(7, 1), new Knight(WHITE));
+	board.set(Position(7, 6), new Knight(WHITE));
+	board.set(Position(0, 1), new Knight(BLACK));
+	board.set(Position(0, 6), new Knight(BLACK));
+	board.set(Position(7, 2), new Bishop(WHITE));
+	board.set(Position(7, 5), new Bishop(WHITE));
+	board.set(Position(0, 2), new Bishop(BLACK));
+	board.set(Position(0, 5), new Bishop(BLACK));
+	board.set(Position(7, 3), new Queen(WHITE));
+	board.set(Position(0, 3), new Queen(BLACK));
+	board.set(Position(7, 4), new King(WHITE));
+	board.set(Position(0, 4), new King(BLACK));
+}
